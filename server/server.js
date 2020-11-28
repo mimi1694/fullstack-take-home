985641df98406ca210a610380e011783719004db
Mimi Scott <mimiscott94@gmail.com>
2020-11-28 16:58:05 -0500

    Made some edits to server.js and started playing around with docker stuff - docker still not working, spin up server.js manually

--- server/server.js
+++ server/server.js
@@ -2,27 +2,34 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const port = 8080;
+const port = 8081;
 const app = express();
 const db = require('./db');
 
-var corsOptions = {
-  origin: 'http://localhost:8081'
+const test = true;
+
+const setUpApp = () => {
+	app.use(cors());
+	app.use(bodyParser.json());
+	app.use(bodyParser.urlencoded({ extended: true }));
+	app.use('/api', require('./routes'));
+
+	app.get('/', (req, res) => {
+		res.status(200).send('Hello World!!');
+	});
+
+	app.listen(port, () => {
+		console.log(`Course Signup Server listening at http://localhost:${port}`);
+	});
+}
+
+
+const setUpDB = () => {
+	// if (test) { }
 };
 
-app.use(cors(corsOptions));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/api', require('./routes'));
-
-app.get('/', (req, res) => {
-	res.status(200).send('Hello World!!');
-});
-
-app.listen(port, () => {
-	console.log(`Course Signup Server listening at http://localhost:${port}`);
-});
-
-db.sync({ force: true }).then(() => {
-  console.log('Dropped and re-sync db.');
+db.sync({ force: test }).then(() => {
+	test && console.log('Dropped and re-sync db.');
+	setUpApp();
+	setUpDB();
 }).catch(console.error);
