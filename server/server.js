bd991c1aa3b0c5a906ff2cb1e678eba6d584333c
Mimi Scott <mimiscott94@gmail.com>
2020-11-30 21:10:10 -0500

    Updated server port to 8080

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const port = 8081;
+const port = 8080;
 const app = express();
 const db = require('./db');
 
